883d3b1ebe1a7e2d44730ba1608eab7082dd9964
gaebor <gaebor@gmail.com>
2016-05-12 10:15:14 +0200

    changed files

---
+++ tests/wordcount_1.py
@@ -0,0 +1,13 @@
+import sys
+from collections import defaultdict
+
+words=defaultdict(lambda: 0)
+
+for line in sys.stdin:
+    for word in line.strip().split():
+        words[word] += 1
+
+words = dict(words)
+
+for word, freq in words.iteritems():
+    print word, freq
